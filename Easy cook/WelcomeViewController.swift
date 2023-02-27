343f8da3f06dddf013a7689d2e0c0b1ac8aa105a
Sergey Azimov <74828253+FarbosZ@users.noreply.github.com>
2023-02-27 16:43:40 +0200

    fix WelcomeVC. add 2 fonts  and UIFont + Extensions (+1 squashed commit) Squashed commits: [d55a1c8] add WelcomeViewController,WelcomeView and backgroundImageWC

---
+++ Easy cook/WelcomeViewController.swift
@@ -0,0 +1,60 @@
+//
+//  WelcomeViewController.swift
+//  Easy cook
+//
+//  Created by Sergey Azimov on 27.02.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    private let welcomeView = WelcomeView()
+    
+    private  let getStartedButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .red
+        button.titleLabel?.textColor = .white
+        button.titleLabel?.font = .poppinsBold16()
+        button.setTitle("Get started", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonTapped(button:)), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    override func loadView() {
+        view = welcomeView
+        view.addSubview(getStartedButton)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadView()
+        setConstraints()
+    }
+    
+    @objc private func buttonTapped(button: UIButton) {
+         let mainVC = ViewController()
+         mainVC.modalPresentationStyle = .fullScreen
+        present(mainVC, animated: true)
+    }
+    
+}
+
+//MARK: - SetConstraints
+
+extension WelcomeViewController {
+   private func setConstraints() {
+        NSLayoutConstraint.activate([
+            getStartedButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80),
+            getStartedButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            getStartedButton.widthAnchor.constraint(equalToConstant: 150),
+            getStartedButton.heightAnchor.constraint(equalToConstant: 60),
+            
+        ])
+    }
+}
