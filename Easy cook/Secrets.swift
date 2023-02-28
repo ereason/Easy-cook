49265de01646006037bf0170ae077282d60df39d
George Epifantssev <Epifantsev.george@gmail.com>
2023-02-28 21:26:27 +0700

    Add Secrets file

---
+++ Easy cook/Secrets.swift
@@ -0,0 +1,15 @@
+//
+//  Secrets.swift
+//  Easy cook
+//
+//  Created by George on 28.02.2023.
+//
+
+import Foundation
+
+class Secrets{
+
+    static let API_KEY = "Enter API key"
+    
+    @available(*, unavailable) private init() {}
+}
